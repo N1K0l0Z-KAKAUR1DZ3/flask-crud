8fee1f0a47ccd1571e0b7c9589f16d685f73763d
N1K0l0Z-KAKAUR1DZ3 <n.kakauridze9@gmail.com>
2023-01-26 17:21:54 +0400

    fix bugs

--- configuration.py
+++ configuration.py
@@ -1,6 +1,6 @@
 from flask import Flask
 import os
-
+from flask_jwt_extended import JWTManager
 
 # Init app
 app = Flask(__name__)
@@ -11,3 +11,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["JWT_SECRET_KEY"] = "super-secret" 
+
+jwt = JWTManager(app)
